ae86264be5a31e3653cc0eb44754f26da624e85e
ed16 <e.zaynullin@gmail.com>
2022-07-06 17:20:26 +0200

    Get filename from CLI

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ed16/hack_asm
+
+go 1.18
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
-package hack_asm
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world23")
+	//Get filename from CLI args
+	var filename string
+
+	if len(os.Args) > 0 {
+		filename = os.Args[1]
+	} else {
+		fmt.Println("Please, specify the file *.hack to be assembled")
+		return
+	}
+
+	i := strings.Index(filename, ".")
+
+	if i == -1 {
+		fmt.Println("Please, specify the file *.hack to be assembled")
+		return
+	}
+
+	if filename[i:] != ".hack" {
+		fmt.Println("Please, specify the file *.hack to be assembled")
+		return
+	}
+
+	fmt.Println(filename)
 }
